697e47aaa46eb82320d755574ebe4146229d0415
Mahfuz Rahman <go2mahfuz@gmail.com>
2026-06-10 14:23:39 -0400

    turned off another browser capability to test minimum

--- src/main/java/com/applitools/utils/EyesManager.java
+++ src/main/java/com/applitools/utils/EyesManager.java
@@ -43,7 +43,7 @@ public class EyesManager {
       //  config.addBrowser(1280, 800, BrowserType.FIREFOX);
       //  config.addBrowser(1280, 800, BrowserType.EDGE_CHROMIUM);
         // Mobile emulation
-        config.addDeviceEmulation(DeviceName.iPhone_14_Pro, ScreenOrientation.PORTRAIT);
+      //  config.addDeviceEmulation(DeviceName.iPhone_14_Pro, ScreenOrientation.PORTRAIT);
         eyes.setConfiguration(config);
         eyes.open(DriverManager.getDriver(), "Login App", testName, new RectangleSize(1280, 800));
         eyesHolder.set(eyes);
